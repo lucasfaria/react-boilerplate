3d4b1668e110d5eea699b7a376b3971a5f8b37cb
Lucas Faria <lucaas.faria@gmail.com>
2021-08-27 07:29:32 -0300

    Estilos do primeiro componente-fix jest versao SC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,8 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>React Avançado - Boilerplate</title>
         <meta name="description" content="React Avançado - Boilerplate" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
